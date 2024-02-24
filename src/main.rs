ade435bc77031223ff5faf717e8e72538f8c3ea5
OmentaElvis <omentaelvis@gmail.com>
2024-02-24 15:19:18 +0300

    Main returns anyhow result

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use cliargs::parse_args;
-
 pub mod cliargs;
 pub mod config;
 pub mod pom;
 pub mod submodules;
 pub mod templating;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     parse_args();
+    Ok(())
 }
