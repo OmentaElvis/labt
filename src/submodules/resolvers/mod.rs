d0482e61874bac7b82f1a5e043fa5916ae24610f
OmentaElvis <omentaelvis@gmail.com>
2024-03-14 13:26:19 +0300

    Implemented a cache writer on net resolver

--- src/submodules/resolvers/mod.rs
+++ src/submodules/resolvers/mod.rs
@@ -1,11 +1,15 @@
 use std::error::Error;
 use std::fmt::Display;
-use std::io;
+use std::io::{self, BufReader};
+use std::{error::Error, io::BufWriter};
 
 use anyhow::Context;
+use log::warn;
 use reqwest::StatusCode;
 
 use crate::{
+    caching::Cache,
+    caching::CacheType,
     pom::{parse_pom, Project},
 };
 
@@ -89,8 +93,53 @@ impl Resolver for NetResolver {
         })?;
 
         if res.status().is_success() {
-            let reader = io::BufReader::new(res);
-            let p = parse_pom(reader, project.to_owned()).map_err(|err| {
+            let mut reader = io::BufReader::new(res);
+            let mut cache = Cache::new(
+                project.get_group_id(),
+                project.get_artifact_id(),
+                project.get_version(),
+                CacheType::POM,
+            );
+
+            let parse_result = if let Err(err) = cache.use_labt_home() {
+                // if we are unable to initialize cache file, just ignore it.
+                // TODO have an effective way to error on this
+                warn!("Unable to cache response \n {:?}", err);
+                parse_pom(reader, project.to_owned())
+            } else {
+                // labt home exists so it looks good to pipe everything to cache and return handle to cache
+                // no need to check if file exists since its a network resolver anyway
+
+                let mut writer = BufWriter::new(Cache::from(&cache).create().map_err(|err| {
+                    ResolverError::new(
+                        "Failed to create cache file",
+                        ResolverErrorKind::Internal,
+                        Some(err.into()),
+                    )
+                })?);
+                std::io::copy(&mut reader, &mut writer).map_err(|err| {
+                    ResolverError::new(
+                        "Failed to copy network contents to cache file",
+                        ResolverErrorKind::Internal,
+                        Some(err.into()),
+                    )
+                })?;
+                drop(writer);
+
+                let cache = cache.open().map_err(|err| {
+                    ResolverError::new(
+                        "Failed to open cache file",
+                        ResolverErrorKind::Internal,
+                        Some(err.into()),
+                    )
+                })?;
+
+                let reader = BufReader::new(cache);
+
+                parse_pom(reader, project.to_owned())
+            };
+
+            let p = parse_result.map_err(|err| {
                 ResolverError::new(
                     format!("Failed to parse pom file at {}", url).as_str(),
                     ResolverErrorKind::Internal,
